c9c8c1ca1c6eaea5d6e7d7854c5b727d0e150109
Yukesh432 <nsiwansh@gmail.com>
2023-09-25 23:20:25 +0545

    linked list

--- addtwonum.py
+++ addtwonum.py
@@ -12,3 +12,11 @@ class Node:
         self.data= data
         self.next= None
 
+class LinkedList:
+    def __init__(self):
+        self.head= None
+
+    def append(self, data):
+        new_node= Node(data)
+        if not self.head:
+            self.head= new_node
