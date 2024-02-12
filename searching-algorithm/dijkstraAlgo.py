5224b3d5d8950db8b8c5350a88ba13a28edbaded
Yukesh432 <nsiwansh@gmail.com>
2024-02-13 00:14:12 +0545

    update:weighted graph

--- searching-algorithm/dijkstraAlgo.py
+++ searching-algorithm/dijkstraAlgo.py
@@ -14,11 +14,20 @@ import networkx as nx
 
 graph= nx.DiGraph()
 graph.add_nodes_from(['Abc','B', 'C','D'])
-graph.add_edge(7, "llo", 0.9)
-graph.add_edge(88, "B", 0.6)
+graph.add_edge(7, "llo")
+graph.add_edge(88, "B")
 edges = [("B", "C", 1.2), ("C", "D", 3.4), ("A", "D", 2.6)]
 graph.add_weighted_edges_from(edges)
 print(graph.number_of_edges())
 print(graph.number_of_nodes())
 print(graph.nodes)
 print(graph.edges)
+
+import matplotlib.pyplot as plt
+
+pos = nx.spring_layout(graph)  # positions for all nodes
+nx.draw(graph, pos, with_labels=True)
+edge_labels = nx.get_edge_attributes(graph, "weight")
+nx.draw_networkx_edge_labels(graph, pos, edge_labels=edge_labels)
+
+plt.show()
