17e60d4f0e7941c74d2e88737e7a6eb44ef8c93b
Yukesh432 <nsiwansh@gmail.com>
2024-03-23 01:35:50 +0545

    update:added docstring

--- sorting-algorithm/bubblesort.py
+++ sorting-algorithm/bubblesort.py
@@ -1,37 +1,38 @@
-import random
 import numpy as np
 
-# creating a list of random number
-list1= np.random.randint(1,10, 5)
-# list2= np.random.randint(20,30, 5)
-print("before sorting........\n")
-print(list1)
-sorted_list= []
+def bubble_sort(arr):
+    """
+    Perform Bubble Sort on an array.
 
-for i in range(0, len(list1)+1):
-    for item in list1:
-        if list1[i]> list1[i+1]:
-            pass
-        else:
-            temp= list1[i+1]
-            list1[i]= temp
-            list1[i+1]= list1[i]
-    print(i)
-    # if list1[i+1]>list1[i]:
-    #     temp= list1[i+1]
-    #     list1[i]= temp
-    #     list1[i+1]= list1[i]
-    #     # sorted_list.append(list1[i+1])
-    # else:
-    #     pass
-        # sorted_list.append(list1[i])
+    This function sorts an array in ascending order using the Bubble Sort algorithm.
+    It iteratively compares adjacent elements and swaps them if they are in the wrong order,
+    ensuring that the largest unsorted element is moved to its correct position in each iteration.
 
+    Parameters
+    ----------
+    arr : ndarray
+        The numpy array to be sorted in place.
 
-#     if list1[index]> list1[index+1]:
-#         sorted_list.append(list1[index])
-#     else:
-#         sorted_list.append(list1[index+1])
+    Returns
+    -------
+    None
+        The function sorts the array in place and returns nothing.
+        """
+    
+    n = len(arr)
+    for i in range(n-1):
+        for j in range(n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
 
-print("List after Bubble sort::")
-print(list1)
+# Example usage
+if __name__ == "__main__":
+    # creating a list of random numbers
+    list1 = np.random.randint(1, 10, size=5)
+    print("Before sorting:\n", list1)
+    
+    # Sorting the list
+    bubble_sort(list1)
+    
+    print("List after Bubble sort:\n", list1)
 
