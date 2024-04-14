61c8022979ebd07984ebc4b458d58caefb273b79
Yukesh432 <nsiwansh@gmail.com>
2024-04-14 19:52:44 +0545

    update:added problem statement

---
+++ searching-algorithm/binary-search.py
@@ -0,0 +1,6 @@
+"""Binary search repeatedly halves the search space by comparing the search key with the middle element of the list. This significantly reduces
+ the number of comparisons needed to find the target element compared to a linear search.
+
+- Faster than linear search for sorted lists.
+- Time complexity: O(log n) in the average and best cases.
+"""
