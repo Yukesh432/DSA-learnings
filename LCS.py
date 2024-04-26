c0928c7f5bc89751fc2ce8f3c8a95564110fd322
Yukesh432 <nsiwansh@gmail.com>
2024-04-26 23:09:42 +0545

    start

--- LCS.py
+++ LCS.py
@@ -8,3 +8,5 @@ The most common method to solve the LCS problem is through dynamic programming b
 optimal substructure and overlapping subproblems. The idea is to build a 2D table 
 L where L[i][j] represents the length of the LCS of the sequences X[1..i] and Y[1..j]
 """
+
+sequence1= ['abc', 'defff', 'ijjkdef']
