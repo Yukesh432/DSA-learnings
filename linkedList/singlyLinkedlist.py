d63285f32f153e4b7fb1636646eb8edea5353776
Yukesh432 <nsiwansh@gmail.com>
2024-06-01 14:33:02 +0545

    insertion at the end

--- linkedList/singlyLinkedlist.py
+++ linkedList/singlyLinkedlist.py
@@ -26,6 +26,22 @@ class LinkedList:
         newNode.next= self.head
         # move the head to point to the new node
         self.head= newNode
+
+    #insertion at the end
+    def append(self, newdata):
+        newNode= Node(newdata)
+        newNode.next= None
+
+        # if the linked list is empty, make the new node as head
+        if self.head is None:
+            self.head= newNode
+            return
+        
+        last= self.head
+        while(last.next):
+            last= last.next
+
+        last.next= newNode
     
 def traverse_link_list(head):
     # start from the head of the linked list
@@ -49,6 +65,7 @@ ll1= LinkedList()
 ll1.push(1)
 ll1.push(34)
 ll1.push(77)
-
+ll1.append(121)
 
 print(ll1.findLength())
+# print(ll1)
