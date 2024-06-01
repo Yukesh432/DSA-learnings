b8916d80049e6c9356a20dce4404d4e14aa221c9
Yukesh432 <nsiwansh@gmail.com>
2024-06-01 20:33:45 +0545

    insertion

--- linkedList/singlyLinkedlist.py
+++ linkedList/singlyLinkedlist.py
@@ -19,7 +19,7 @@ class LinkedList:
         return length
     
     # function to add new node at the front of the list
-    def push(self, newdata):
+    def push_at_front(self, newdata):
 
         newNode= Node(newdata)
         # make next of the new node as head
@@ -28,9 +28,8 @@ class LinkedList:
         self.head= newNode
 
     #insertion at the end
-    def append(self, newdata):
+    def append_at_last(self, newdata):
         newNode= Node(newdata)
-        newNode.next= None
 
         # if the linked list is empty, make the new node as head
         if self.head is None:
@@ -38,21 +37,23 @@ class LinkedList:
             return
         
         last= self.head
-        while(last.next):
+        while last.next:
             last= last.next
-
         last.next= newNode
-    
-def traverse_link_list(head):
-    # start from the head of the linked list
-    current= head
-    while current is not None:
-        print(current.data)
 
-        current= current.next
+    def traverse_link_list(self):
+        # start from the head of the linked list
+        current= self.head
+        while current is not None:
+            print(current.data)
+
+            current= current.next
 
 
-# head= Node(22)
+head= Node(22)
+head.next= Node(33)
+# head.next.next= Node(44)
+
 # second= Node(33)
 # third= Node(44)
 
@@ -62,10 +63,13 @@ def traverse_link_list(head):
 # traverse_link_list(head)
 
 ll1= LinkedList()
-ll1.push(1)
-ll1.push(34)
-ll1.push(77)
-ll1.append(121)
+# ll1.head= 676
+# ll1.push_at_front(1)
+# ll1.push_at_front(34)
+# ll1.push_at_front(77)
+ll1.append_at_last(121)
 
-print(ll1.findLength())
+# print(ll1.findLength())
+ll1.traverse_link_list()
+
 # print(ll1)
