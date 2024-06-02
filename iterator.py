7aa4b139310afb5425cefcbc502360f790884095
Yukesh432 <nsiwansh@gmail.com>
2024-06-02 22:04:08 +0545

    iter and next

---
+++ iterator.py
@@ -0,0 +1,36 @@
+ids= [1,2,3,4,5]
+
+# for id in ids:
+#     print(id)
+#     print(dir(id))
+#     break
+# print("*"*100)
+# print(dir(ids))
+# print("*"*100)
+
+nums= ids.__iter__()
+# nums= iter(ids)
+# print("*"*100)
+# print(nums)
+# print("*"*100)
+# print(dir(nums))
+# print("*"*100)
+# print(nums.__next__())
+# print("#"*100)
+# print(iter(nums))
+
+# print(next(iter(ids)))
+# print(next(iter(ids)))
+# print(next(iter(ids)))
+# print(next(iter(ids)))
+
+print(next(nums))
+print(next(nums))
+print(next(nums))
+print(next(nums))
+print(next(nums))
+print(next(nums))
+print("*"*100)
+
+# print(ids.__iter__().__next__())
+# print(ids.__iter__().__next__())
