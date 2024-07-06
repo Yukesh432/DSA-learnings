892a39a21ecdd543b16395b1fdfdff7814afe915
yukesh-ix <yukesh.nepal@olivegroup.io>
2024-07-06 09:23:24 +0545

    removed imports

---
+++ pythonBasics/closures.py
@@ -0,0 +1,14 @@
+def factorial(n):
+    if n==0:
+        return 1
+
+    return n*factorial(n-1)
+
+print(factorial(6))
+
+print(factorial.__doc__)
+
+
+print(dir(factorial))
+
+print(factorial.__dict__)
