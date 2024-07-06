091a0f8538a205ed5b209b3cc0e59d081731347a
yukesh-ix <yukesh.nepal@olivegroup.io>
2024-07-06 22:18:32 +0545

    bobo query

--- pythonBasics/closures.py
+++ pythonBasics/closures.py
@@ -1,14 +1,20 @@
-def factorial(n):
-    if n==0:
-        return 1
+# def factorial(n):
+#     if n==0:
+#         return 1
 
-    return n*factorial(n-1)
+#     return n*factorial(n-1)
 
-print(factorial(6))
+# print(factorial(6))
 
-print(factorial.__doc__)
+# print(factorial.__doc__)
 
 
-print(dir(factorial))
+# print(dir(factorial))
 
-print(factorial.__dict__)
+# print(factorial.__dict__)
+
+import bobo
+
+@bobo.query('/')
+def hello(person):
+    return 'Hello %s!' %person
