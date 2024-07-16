2f5fe94bb0eb1df6067a15c87c62dd69b57abbd2
yukesh432 <nsiwansh@gmail.com>
2024-07-17 00:03:14 +0545

    namedtuple type

--- pythonBasics/collectionss.py
+++ pythonBasics/collectionss.py
@@ -7,6 +7,9 @@ Flat Sequence: str, byte, bytearray, memoryview, array.array can hold items of o
 # Defining and using namedtuple type
 from collections import namedtuple
 
+# Here the first argument should be the name of the namedtuple type and second argument should be a string
+# contatining the names of the fields separated by spaces or commas
+
 Food= namedtuple('FoodItems', 'name ingredients chemicalcomposition weight')
 
 oats= Food('Instant-oats', 'barley', "protein fibers", 100)
