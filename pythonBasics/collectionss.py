a947a8ce768351caab186efaf5f8babb72c752f3
yukesh432 <nsiwansh@gmail.com>
2024-07-16 23:46:15 +0545

    doc

--- pythonBasics/collectionss.py
+++ pythonBasics/collectionss.py
@@ -1,1 +1,8 @@
+"""
+Container Sequence: list, tuple, collections.deque can hold items of different types of sequence
+
+Flat Sequence: str, byte, bytearray, memoryview, array.array can hold items of one type only
+"""
+
 from collections import defaultdict
+
