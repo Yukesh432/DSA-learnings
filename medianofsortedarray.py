9b442c38bc57a7152feed33313d7106ab14e4e39
yukesh432 <nsiwansh@gmail.com>
2024-08-02 23:10:08 +0545

    import

--- medianofsortedarray.py
+++ medianofsortedarray.py
@@ -16,7 +16,6 @@ Consider arrays `nums1 = [1, 3]` and `nums2 = [2]`.
 If we merge them, we get `[1, 2, 3]`. The median in this case is the 
 middle element, which is 2.
 """
-import math
 
 a= [20, 30, 40, 50]
 b= [3, 5, 7, 9]
