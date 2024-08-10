2eb40f4c13f311a6a5cc1990e60d5184228884da
yukesh432 <nsiwansh@gmail.com>
2024-08-10 21:05:49 +0545

    botoclient

--- cloudd/lambdaa.py
+++ cloudd/lambdaa.py
@@ -1,3 +1,6 @@
+import boto3
+import botocore
+
 import random
 
 def random_drink():
@@ -5,3 +8,10 @@ def random_drink():
     return random.choice(drink)
 
 print(random_drink())
+
+def lambda_handler(event, context):
+   print(f'boto3 version: {boto3.__version__}')
+   print(f'botocore version: {botocore.__version__}')
+
+if __name__=='__main__':
+    lambda_handler('fd', 'test')
